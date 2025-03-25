0e9d78b6d39ea76a20e48408fef1753b2c70bfb8
angelesortega <mariaortega5l@outlook.com>
2025-03-25 20:16:18 +0100

    guardando calculadora

---
+++ src/main/java/com/maol/demo/calculadora/service/Calculadora.java
@@ -0,0 +1,26 @@
+package com.maol.demo.calculadora.service;
+
+public class Calculadora {
+
+	public int suma(int parametroNumero1, int parametroNumero2) {
+		return parametroNumero1 + parametroNumero2;
+	}
+
+	
+	public int resta(int parametroNumero1, int parametroNumero2) {
+		return parametroNumero1 - parametroNumero2;
+	}
+	
+	
+	public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		int parametroNumero1 = 2;
+		int parametroNumero2 = 3;
+
+		System.out.println(calculadora.suma(parametroNumero1, parametroNumero2));
+		System.out.println(calculadora.suma(3, 3));
+		
+		System.out.println(calculadora.resta(2,2));
+	}
+
+}
