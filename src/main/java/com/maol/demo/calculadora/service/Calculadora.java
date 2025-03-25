231302fa9fc211d6cd3bad6d565a63b65145c8aa
angelesortega <mariaortega5l@outlook.com>
2025-03-25 20:29:49 +0100

    guardando por fin calculadora sumar restar

--- src/main/java/com/maol/demo/calculadora/service/Calculadora.java
+++ src/main/java/com/maol/demo/calculadora/service/Calculadora.java
@@ -6,12 +6,10 @@ public class Calculadora {
 		return parametroNumero1 + parametroNumero2;
 	}
 
-	
 	public int resta(int parametroNumero1, int parametroNumero2) {
 		return parametroNumero1 - parametroNumero2;
 	}
-	
-	
+
 	public static void main(String[] args) {
 		Calculadora calculadora = new Calculadora();
 		int parametroNumero1 = 2;
@@ -19,8 +17,9 @@ public class Calculadora {
 
 		System.out.println(calculadora.suma(parametroNumero1, parametroNumero2));
 		System.out.println(calculadora.suma(3, 3));
-		
-		System.out.println(calculadora.resta(2,2));
+
+		System.out.println(calculadora.resta(2, 2));
+		System.out.println(calculadora.resta(parametroNumero1, parametroNumero2));
 	}
 
 }
