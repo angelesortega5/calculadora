74b882205f434b78ebe41571cd7d12d567acc652
angelesortega <mariaortega5l@outlook.com>
2025-03-26 08:28:11 +0100

    guardando calculadora multiplicar

--- src/main/java/com/maol/demo/calculadora/service/Calculadora.java
+++ src/main/java/com/maol/demo/calculadora/service/Calculadora.java
@@ -10,6 +10,11 @@ public class Calculadora {
 		return parametroNumero1 - parametroNumero2;
 	}
 
+	public int multiplicar(int parametroNumero1, int parametroNumero2) {
+		return parametroNumero1 * parametroNumero2;
+	}
+
+
 	public static void main(String[] args) {
 		Calculadora calculadora = new Calculadora();
 		int parametroNumero1 = 2;
@@ -20,6 +25,7 @@ public class Calculadora {
 
 		System.out.println(calculadora.resta(2, 2));
 		System.out.println(calculadora.resta(parametroNumero1, parametroNumero2));
+
 	}
 
 }
